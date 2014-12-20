81df6d0f08278f0275de80f51fb0019608a1c27d
Prakhar Shukla <prafiles@yahoo.com>
2014-12-20 23:06:57 +0530

    Initial Commit

---
+++ pkg-remove/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pkg-remove
+//
+//  Created by Prakhar Shukla on 20/12/14.
+//  Copyright (c) 2014 Prakhar Shukla. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
